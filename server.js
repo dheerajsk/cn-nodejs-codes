b012ccd30a20ed8177f6aa4caeae9cc29dedbbd5
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2023-06-16 11:29:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const server = http.createServer((req, res) => {
 });
 
 // 3. Specify a port to listen to client's requests.
-server.listen(3100);
+server.listen(3100, () => {
+  console.log('Server is listening on port 3100');
+});
 
-console.log('Server is listening on port 3100');
+// console.log('Server is listening on port 3100');
