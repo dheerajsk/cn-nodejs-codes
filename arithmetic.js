054dddb53decb2d18f1e6d1272a09f5d20ac2e4a
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2023-06-16 11:46:24 +0530

    Update arithmetic.js

--- arithmetic.js
+++ arithmetic.js
@@ -1,10 +1,16 @@
-module.exports.sum = (x, y) => {
+exports.sum = (x, y) => {
   return x + y;
 };
 
 function div(x, y) {
   return x / y;
 }
+
+console.log('Loading arithmetic');
+
+// exports = function (x, y) {
+//   return x + y;
+// };
 
 // 1. Common JS Module
 
