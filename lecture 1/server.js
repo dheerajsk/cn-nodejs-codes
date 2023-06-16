671e3efb2a960121edc214191c2613a087f0a962
Dheeraj Kumarclear <dheeraj.s.roy@gmail.com>
2023-06-16 12:22:35 +0530

    global

--- lecture 1/server.js
+++ lecture 1/server.js
@@ -6,10 +6,10 @@ const http = require('http');
 // 2. Create Server.
 const server = http.createServer((req, res) => {
   // Here comes the request.
-  res.end('Welcome to NodeJS Ninja Server');
+  res.end('Welcome to NodeJS Ninja Servers.');
 });
 
 // 3. Specify a port to listen to client's requests.
 server.listen(3100);
 
-console.log('Server is listening on port 3100');
+console.log('Server is listening on port 3100.');
