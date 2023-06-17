1530297e7fef34a9d2d87bbb138d009db9866bb4
Dheeraj Kumarclear <dheeraj.s.roy@gmail.com>
2023-06-17 13:12:51 +0530

    server

--- express-server.js
+++ express-server.js
@@ -1,36 +1,15 @@
-const express = require('express')
+const express = require('express');
 
 // Create a server.
 
-const server = express()
+const server = express();
 
 // Handle default request.
-
-server.get('/', (req, res, next) => {
-  console.log('first middleware hit')
-  next()
-})
-
-// GET request
-
 server.get('/', (req, res) => {
-  res.set('Content-Type', 'text/plain')
-  res.send('Hello world! This is Express server')
-})
+  res.send('Welcome to Express Server');
+});
 
-server.post('/', (req, res) => {
-  res.status(201).send('Post request received')
-})
-
-server.put('/', (req, res) => {
-  res.send('Put request received')
-})
-
-server.delete('/', (req, res) => {
-  res.send('Delete request received')
-})
-
-// Assign port
-server.listen(3200, () => {
-  console.log(`Server running at http://localhost:3200`)
-})
+// Listen on specified port.
+server.listen(3100, () => {
+  console.log('Server is listening on 3100');
+});
