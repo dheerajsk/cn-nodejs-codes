4b254334e1450da4c9508fa2c9595c4c28d08498
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2023-06-18 12:41:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,15 @@ const http = require('http');
 
 // 2. Create Server.
 const server = http.createServer((req, res) => {
-  // Here comes the request.
-  res.end('Welcome to NodeJS Ninja Server');
+  console.log(req.url);
+  res.write('Welcome to my application');
+  if (req.url == '/product') {
+    // respone ends
+    res.end('This is Product Page');
+  } else if (req.url == '/user') {
+    res.end('This is User page');
+  }
+  // logging.
 });
 
 // 3. Specify a port to listen to client's requests.
