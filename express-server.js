e54e270af363cb66073e5a246b7cc45e2d3c5364
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2023-07-08 20:11:04 +0530

    middlewares

--- express-server.js
+++ express-server.js
@@ -5,11 +5,17 @@ const express = require('express');
 const server = express();
 
 // Handle default request.
+
+server.get('/', (req, res, next) => {
+  res.send('first middleware hit')
+  next()
+});
+
 server.get('/', (req, res) => {
-  res.send('Welcome to Express Server');
+  res.send('Hello world! This is Express Server');
 });
 
 // Listen on specified port.
 server.listen(3100, () => {
-  console.log('Server is listening on 3100');
+  console.log('Server is listening at http://localhost:3200');
 });
